490f8838c171b534a0de61e7c12ae196ae98b0e3
RafaPalau <rafaeldonizetip@gmail.com>
2021-05-10 20:13:34 -0300

    modal created

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import logo from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface Headerprops {
+  onOpenNewTransactionModal: () => void; // Essa propriedade é uma função, e o retorno dessa função é vazio.
+}
+
+export function Header({ onOpenNewTransactionModal }: Headerprops) {
   return (
     <Container>
       <Content>
         <img src={logo} alt='dt money' />
-        <button type='button'>Nova transação</button>
+        <button onClick={onOpenNewTransactionModal} type='button'>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
