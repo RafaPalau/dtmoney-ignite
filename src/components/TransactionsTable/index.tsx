df3639df7b8d80b0b77aaefd29011cf98b9241eb
RafaPalau <rafaeldonizetip@gmail.com>
2021-05-10 18:49:43 -0300

    install e config MirageJs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -15,13 +21,13 @@ export function TransactionsTable() {
         <tbody>
           <tr>
             <td>Desenvolvimento de WebSite</td>
-            <td className="deposit">R$12.000</td>
+            <td className='deposit'>R$12.000</td>
             <td>Desenvolvimento</td>
             <td>20/02/2021</td>
           </tr>
           <tr>
             <td>Aluguel</td>
-            <td className="withdraw">-R$1.100</td>
+            <td className='withdraw'>-R$1.100</td>
             <td>Casa</td>
             <td>17/02/2021</td>
           </tr>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
+import { App } from "./App";
 
+createServer({
+  // Quais rotas teremos na API ficticia
+  routes() {
+    // As nossas chamadas serão a partir
+    // de api
+    this.namespace = "api";
+
+    // para cada uma das rotas faremos devolveremos alguma coisa
+    // Nesse exemplo um objeto com alguns dados. 
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
